e0a39beb8d664f8a536d8fc10450ee3a0b9ccce3
sussica <sussicaya@gmail.com>
2019-05-01 05:24:49 -0700

    Finished the front end and used local host to host it

--- app.js
+++ app.js
@@ -8,4 +8,11 @@ app.use(bodyParser.urlencoded({
 }))
 const port = 3000
 
+app.use(express.static("local"))
+
 app.listen(port, () => console.log('Server is running on port 3000'))
+
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/signup.html')
+})

06cb37782a489faed792ccebf6ad23e7e579bd8b
sussica <sussicaya@gmail.com>
2019-05-01 05:37:30 -0700

    Finished GET and POST request handle

--- app.js
+++ app.js
@@ -16,3 +16,11 @@ app.listen(port, () => console.log('Server is running on port 3000'))
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/signup.html')
 })
+
+app.post('/', function(req,res){
+  var firstname = req.body.fname;
+  var lastname = req.body.lname;
+  var email = req.body.email;
+  var password = req.body.password;
+  console.log(firstname+lastname+email+password);
+})

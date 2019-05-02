86fbe70483547dfdd6f542310b0114d18108a4df
sussica <sussicaya@gmail.com>
2019-05-02 05:44:29 -0700

    Finished success and failure page

--- app.js
+++ app.js
@@ -17,10 +17,48 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/signup.html')
 })
 
-app.post('/', function(req,res){
+app.post('/', function(req, res) {
   var firstname = req.body.fname;
   var lastname = req.body.lname;
   var email = req.body.email;
   var password = req.body.password;
-  console.log(firstname+lastname+email+password);
-})
+  console.log(firstname + lastname + email + password);
+
+  var data = {
+    members: [{
+      "email_address": email,
+      "status": "subscribed",
+      "merge_fields": {
+        "FNAME": firstname,
+        "LNAME": lastname
+      },
+    }]
+  }
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: 'https://us20.api.mailchimp.com/3.0/lists/bf017a8bd4',
+    method: "POST",
+    headers: {
+      "Authorization": 'Scarlett dce7fdff747dc10b7bfac7c2fcf7b32b-us20'
+    },
+    body: jsonData
+  }
+
+
+  request(options, function(error, response, body) {
+    if (error) {
+      res.sendFile(__dirname + '/failure.html');
+    } else if (response.statusCode == 200) {
+      res.sendFile(__dirname + '/success.html');
+    } else {
+      res.sendFile(__dirname + '/failure.html');
+    }
+  });
+
+  app.post('/failure', function(req, res) {
+    res.redirect("/");
+  });
+
+});
